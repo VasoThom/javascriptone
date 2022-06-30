d19a012ed4348e5d93ce958d50a12f7af83d7e66
Vaso <vasothom@hotmail.com>
2022-06-30 08:59:40 +0200

    javascript

---
+++ index.js
@@ -0,0 +1,49 @@
+let vorname = "Vaso";
+let land = "Greece";
+console.log("Hallo ich bin " + vorname + " und ich komme aus " + land + ".");
+
+let essen = "mousakas";
+let essenZwei = "Pastitsio";
+console.log("Meine Lieblingsspeisen sind " + essen + " und " + essenZwei + ".");
+
+let alter = 32;
+let geburtsJahr = 1990;
+console.log("Ich bin " + alter + " Jahre alt und " + geburtsJahr + "geboren.");
+
+// aufgabe B
+
+// A
+const wasserTief = 300;
+const wassergesamt = 3750;
+
+const wasserinzentimeter = wassergesamt / wasserTief;
+const wasserinfunfZentimetern = wasserinzentimeter * 5;
+console.log(wasserinfunfZentimetern);
+
+// B
+const km = 100;
+const tanksFüllung = 66;
+const liter = 8;
+const kilometer = (km * tanksFüllung) / liter;
+console.log(kilometer);
+
+// C
+const stunden = 17;
+const arbeiter = 3;
+const sumStunden = (stunden * arbeiter) / 5;
+console.log(sumStunden);
+
+// D
+const sum = 882 / (14 * 12) / (2.3 * 999);
+console.log(sum);
+
+// E
+const summ = (12 * 7 * 17) / (7 / 17) + 333;
+console.log(summ);
+
+// F
+const grundstuck = 1680; //cm
+const socken = 120; //cm
+const anzahl = grundstuck / socken;
+const kostenalle = anzahl * 24.4;
+console.log(kostenalle);
